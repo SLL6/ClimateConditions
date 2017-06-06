0e7bff8575a42c7c811d6a719688753a4fff3bac
Scott Lim <Scott Lim>
2017-06-05 23:18:30 -0700

    help

--- public/javascript/weather.js
+++ public/javascript/weather.js
@@ -5,7 +5,7 @@ console.log(httpGet());
 function httpGet()
 {
     var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://api.openweathermap.org/data/2.5/weather?q=London&mode=json", true);
+xhr.open("GET", "http://samples.openweathermap.org/data/2.5/forecast?id=524901&appid=" + KEY, true);
 xhr.onload = function (e) {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
