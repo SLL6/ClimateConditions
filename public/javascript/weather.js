0ce7f49b6ac677021c718eb1a71f31e9aa249f83
Scott Lim <Scott Lim>
2017-06-05 23:01:37 -0700

    Try again

--- public/javascript/weather.js
+++ public/javascript/weather.js
@@ -4,7 +4,7 @@ console.log(httpGet());
 function httpGet()
 {
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", "http://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1", false ); // false for synchronous request
+    xmlHttp.open( "GET", "https://samples.openweathermap.org/data/2.5/weather?zip=94040,us&appid=b1b15e88fa797225412429c1c50c122a1", false ); // false for synchronous request
     xmlHttp.send( null );
     return xmlHttp.responseText;
 }
