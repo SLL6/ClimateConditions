7a76e173e860c977e0029aa39c770786dad03ae3
Scott Lim <push.default>
2018-04-11 11:58:25 -0700

    testing if can call weather api

--- public/javascript/weather.js
+++ public/javascript/weather.js
@@ -1,18 +1,17 @@
-var KEY = "35ec5bf920b736305714f59eed2a6f0a";
+var KEY = "e4f143fbdf05222d";
 
 console.log(httpGet());
 
 function httpGet()
 {
     var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://api.wunderground.com/api/e4f143fbdf05222d/conditions/q/CA/San_Francisco.json", true);
+xhr.open("GET", "http://api.wunderground.com/api/e4f143fbdf05222d/conditions/q/CA/San_Diego.json", true);
 //xhr.setRequestHeader("Access-Control-Allow-Origin", "localhost:5000");
 xhr.onload = function (e) {
   if (xhr.readyState === 4) {
     if (xhr.status === 200) {
       console.log(xhr.responseText);
-      var response = JSON.parse(xhr.responseText);
-      console.log("Temperature(K): " + response.main.temp);
+      //document.getElementById("candy").innerHTML = xhr.responseText;
     } else {
       console.error(xhr.statusText);
     }
